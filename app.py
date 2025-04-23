6767c180932248381cbcd92cbe93a1cb956acba0
Maxwell T <43257438+mtworth@users.noreply.github.com>
2025-04-23 21:58:37 +0000

    turning this into a streamlit app

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import streamlit as st
-print("Hello World")
+
+st.write("This is our data app.")
+
+df = pd.read_csv("Film_Locations_in_San_Francisco_20250423.csv")
+
+st.dataframe(df)
